17a3f5d5f5e3cdf536858eb4447e7a41cad0a956
Auxiguilar <filip.myras@gmail.com>
2026-01-20 10:39:38 +0100

    Made REPL.

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+
+		if cleanedInput == nil {
+			fmt.Println("invalid input")
+		}
+
+		firstWord := cleanedInput[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
